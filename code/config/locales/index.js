749f85d2a866e7cc82fa3ebc9f18f5bd84977003
Cesar Carbajo <cesarcf@ext.inditex.com>
2023-02-13 09:50:24 +0100

    folders locales

---
+++ code/config/locales/index.js
@@ -0,0 +1,10 @@
+import shared from "./shared";
+
+export default {
+  es: {
+    ...shared.es,
+  },
+  en: {
+    ...shared.en,
+  },
+};
---
+++ code/config/locales/shared/index.js
@@ -0,0 +1,7 @@
+import es from "./es";
+import en from "./en";
+
+export default {
+  es: { ...es },
+  en: { ...en },
+};
